39ec3c0b41f9617dde108e5b06e1f543283414c3
jabagdoshvili <jabagdoshvili@gmail.com>
2021-02-05 19:08:22 +0400

    changes

--- js/main.js
+++ js/main.js
@@ -20,35 +20,43 @@ $('.select-container:not(.opened)').click(function (ev) {
 })
 
 
-// $('.opened').click(function () {
+$(document).on('click', function(ev) {
 
-//     $(this).hide()
+    $('.select-container.opened').removeClass('opened')
+    $('.select-body').hide()
+})
 
-// })
-
-// $('body:not(.select-body)').on('click', function(ev) {
-//     $('.select-container.opened').removeClass('opened')
-//     $('.select-body').hide()
-// })
+$('.select-body').on('click', function(ev) {
+    ev.stopPropagation()
+})
 
 $('.compress').click(function () {
     $('nav').toggleClass('compressed')
     $(this).toggleClass('rotated')
+    $('.section-area').toggleClass('compressed')
 })
 
 
 var selectContainers = {
     status: [
         {
+            id: '0',
+            name: 'All'
+        },
+        {
+            id: '1',
             name: 'Free'
         },
         {
+            id: '2',
             name: 'Pending'
         },
         {
+            id: '3',
             name: 'In Progress'
         },
         {
+            id: '4',
             name: 'On the way'
         }
     ]
@@ -62,17 +70,31 @@ function InitSelectContainer(openSelectContainer) {
     }
     selectContainers[openSelectContainer].forEach(el=> {
         var name = el.name
+        var id = el.id
         container += `
             <li>
                 <label for="${name}">
-                <input type="checkbox" name="${name}" id="">
+                <input type="checkbox" name="${name}" id="${id}">
                 <span>${name}</span>
                 </label>
             </li>`
     })
 
+
     $('.select-body ul').html(container)
 }
+
+
+
+$('.select-body').on('change', 'input[name="All"]', function() {
+
+    if ($(this).is(':checked')) {
+        $('.select-body input').attr('checked', true);
+    } else {
+        $('.select-body input').attr('checked', false);
+    }
+
+});
 
 $( ".markers" ).click(function() {
     $(this).siblings('.driver-info').toggleClass('active')
@@ -82,3 +104,11 @@ $(".assign").click(function() {
     $(this).siblings('.order-ids').toggleClass('active')
 });
 
+
+$( ".menu-item:not(.active)" ).mouseover(function() {
+    $(this).parent('li').addClass('active')
+  });
+
+  $( ".menu-item:not(.active)" ).mouseleave(function() {
+    $(this).parent('li').removeClass('active')
+  });
